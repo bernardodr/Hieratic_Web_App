92362a2657aa8fe7489060ee2486fe729bf1cfcc
Daniel Bernardo <db283@st-andrews.ac.uk>
2022-06-23 09:32:18 +0100

    fixed OCR results card placement

--- Client/results.js
+++ Client/results.js
@@ -104,27 +104,34 @@ fetch('/imageName1')
             var muller4 = filename4.split('_')[0];
             var muller5 = filename5.split('_')[0];
 
-
             document.getElementById('imageName1').innerHTML = muller1;
             document.getElementById('imageName2').innerHTML = muller2;
             document.getElementById('imageName3').innerHTML = muller3;
             document.getElementById('imageName4').innerHTML = muller4;
             document.getElementById('imageName5').innerHTML = muller5;
+
+            // get Facsimile Maker 
+            var facsimileMaker1 = filename1;
+            var facsimileMaker2 = filename2;
+            var facsimileMaker3 = filename3;
+            var facsimileMaker4 = filename4;
+            var facsimileMaker5 = filename5;
+
+            // get Provenance
+            var provenance1 = filename1;
+            var provenance2 = filename2;
+            var provenance3 = filename3;
+            var provenance4 = filename4;
+            var provenance5 = filename5;
+
+            // get Text 
+
+            // get Time Period
+
+            // get Author
+
+
+            
             return console.log(data.image2);
     })
     
-   
-
-// fetch('/imageName1')
-//     .then(response => {
-//         console.log(response)
-//         return response;
-//     })
-//     .then(response => {
-//         console.log(response)
-//         return response.toString();
-//     })
-//     .then(string => {
-//         console.log(string)
-//         document.getElementById('imageName1').innerHTML = string;
-//     });
